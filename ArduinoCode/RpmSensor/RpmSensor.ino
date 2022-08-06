964a3b8b49fdf113f4098b0e7d9064f901b1318a
Brian Hooper <brian_hooper@msn.com>
2022-08-06 08:24:48 -0700

    Update RPM sensor

--- ArduinoCode/RpmSensor/RpmSensor.ino
+++ ArduinoCode/RpmSensor/RpmSensor.ino
@@ -1,37 +1,73 @@
-int hall_pin = 2;
-float hall_thresh = 10.0;
-float multiplier = 3.5;
+#define BAUD_RATE 9600
+#define MIN_RPM 0
+#define MAX_RPM 200
+#define HALL_PIN 2
+#define HALL_THRESHOLD 3.0
+#define SENSORS_PER_WHEEL 3.0
+#define MULTIPLIER 0.87
+#define WHEEL_SIZE_RATIO 0.125
+#define TIMEOUT_SECONDS 1.0
+
+float hall_counts_per_measurement = HALL_THRESHOLD / SENSORS_PER_WHEEL;
 
 void setup() {
-    Serial.begin(115200);
-    pinMode(hall_pin, INPUT);
+    Serial.begin(9600);
+    pinMode(HALL_PIN, INPUT);
+}
+
+bool Timeout(float start_time) {
+    float end_time = micros();
+    float seconds_elapsed = ((end_time - start_time) / 1000000.0);
+    return seconds_elapsed >= TIMEOUT_SECONDS;
+}
+
+uint8_t GetRpmShort(float start_time) {
+    float end_time = micros();
+    if (end_time < start_time) {
+        return 0;
+    }
+
+    float time_passed_seconds = (end_time - start_time) / 1000000.0;
+    float revolutions_per_second = hall_counts_per_measurement / time_passed_seconds;
+    float revolutions_per_minute = revolutions_per_second * 60.0;
+    float rpm_temp = revolutions_per_minute * MULTIPLIER * WHEEL_SIZE_RATIO;
+    if (rpm_temp > 10.0) {
+      float rpm_temp_ratio = rpm_temp - 10.0;
+      rpm_temp_ratio = 100.0 * (rpm_temp_ratio / 10.0);
+      rpm_temp = rpm_temp_ratio;
+    }
+    int rpm = rpm_temp;
+    
+    if (rpm < MIN_RPM || rpm > MAX_RPM) {
+        return 0;
+    }
+
+    return (uint8_t) rpm;
+
 }
 
 void loop() {
-    float hall_count = 0.0;
+    float hall_count = 0;
     float start = micros();
     bool on_state = false;
 
-    while(true) {
-        if (digitalRead(hall_pin) == 0) {
-            if (on_state == false){
+    while(hall_count < HALL_THRESHOLD && !Timeout(start)) {
+        if (digitalRead(HALL_PIN) == 0) {
+            if (on_state == false) {
                 on_state = true;
                 hall_count += 1.0;
             }
         } else{
             on_state = false;
         }
-
-        if (hall_count >= hall_thresh) {
-            break;
-        }
     }
 
-    if (hall_count > 0) {
-        float end_time = micros();
-        float time_passed = ((end_time - start) / 1000000.0);
-        float rpm_val = ((hall_count/time_passed) * 60.0) / multiplier;
-        Serial.print(rpm_val);
-        delay(1);
+    if (hall_count >= HALL_THRESHOLD) {
+        uint8_t rpm = GetRpmShort(start);
+        Serial.write(rpm);
     }
+    else {
+        Serial.write((uint8_t)0);
+    }
+    delay(2);
 }
