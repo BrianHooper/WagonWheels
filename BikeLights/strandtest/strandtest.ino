0bec8cdb24368d80946f58a4e84bbd71241cf1cf
Brian Hooper <brian_hooper@msn.com>
2022-08-25 21:03:15 -0700

    Added bike lights

---
+++ BikeLights/strandtest/strandtest.ino
@@ -0,0 +1,31 @@
+#include <Adafruit_NeoPixel.h>
+#define LED_PIN    2
+#define LED_COUNT 50
+
+Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+
+void setup() {
+  strip.begin();
+  strip.show();
+  strip.setBrightness(160); // Set BRIGHTNESS to about 1/5 (max = 255)
+}
+
+void loop() {
+  rainbow(20);
+}
+
+void colorWipe(uint32_t color, int wait) {
+  for(int i=0; i<strip.numPixels(); i++) {
+    strip.setPixelColor(i, color);
+    strip.show();
+    delay(wait);
+  }
+}
+
+void rainbow(int wait) {
+  for(long firstPixelHue = 0; firstPixelHue < 5*65536; firstPixelHue += 256) {
+    strip.rainbow(firstPixelHue);
+    strip.show();
+    delay(wait);
+  }
+}
